85e18ae3d8387eb3b434d493d209bc1a5e7d6a2c
Ishraq Ahmed Jamaluddin <ishraqahmedjamaluddin@gmail.com>
2022-04-19 23:05:27 +0200

    auth

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,115 @@
+<?php
+  
+namespace App\Http\Controllers\Auth;
+  
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Session;
+use App\Models\Customer;
+use Hash;
+  
+class AuthController extends Controller
+{
+    /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public function index()
+    {
+        return view('auth.login');
+    }  
+      
+    /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public function registration()
+    {
+        return view('auth.registration');
+    }
+      
+    /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public function postLogin(Request $request)
+    {
+        $request->validate([
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+   
+        $credentials = $request->only('email', 'password');
+        if (Auth::attempt($credentials)) {
+            return redirect()->intended('dashboard')
+                        ->withSuccess('You have Successfully loggedin');
+        }
+  
+        return redirect("login")->withSuccess('Oppes! You have entered invalid credentials');
+    }
+      
+    /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public function postRegistration(Request $request)
+    {  
+        $request->validate([
+            'fname' => 'required',
+            'lname' => 'required',
+            'email' => 'required|email|unique:customers',
+            'password' => 'required|min:6',
+        ]);
+           
+        $data = $request->all();
+        $check = $this->create($data);
+         
+        return redirect("dashboard")->withSuccess('Great! You have Successfully loggedin');
+    }
+    
+    /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public function dashboard()
+    {
+        if(Auth::check()){
+            return view('dashboard');
+        }
+  
+        return redirect("/")->withSuccess('Opps! You do not have access');
+    }
+    
+    /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public function create(array $data)
+    {
+      return Customer::create([
+        'fname' => $data['fname'],
+        'lname' => $data['lname'],
+        'email' => $data['email'],
+        'password' => Hash::make($data['password'])
+      ]);
+    }
+    
+    /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public function logout() {
+        Session::flush();
+        Auth::logout();
+  
+        return Redirect('/');
+    }
+}
--- database/migrations/2022_04_07_085256_create_customers_table.php
+++ database/migrations/2022_04_07_085256_create_customers_table.php
@@ -15,13 +15,13 @@ class CreateCustomersTable extends Migration
     {
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
-            $table->string('fname')->nullable();
-            $table->string('lname')->nullable();
+            $table->string('fname');
+            $table->string('lname');
             $table->string('email')->unique();
             // $table->timestamp('email_verified_at')->nullable();
-            $table->string('address');
-            $table->string('country');
-            $table->string('phone');
+            $table->string('address')->nullable();
+            $table->string('country')->nullable();
+            $table->string('phone')->nullable();
             $table->string('password');
             $table->timestamps();
         });
