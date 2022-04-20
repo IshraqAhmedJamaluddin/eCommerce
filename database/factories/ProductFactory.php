aea59d57a301f49eaa0f3cee569987b2810341ce
Ishraq Ahmed Jamaluddin <ishraqahmedjamaluddin@gmail.com>
2022-04-20 21:17:56 +0200

    product factory

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->text,
+            'price' => $this->faker->numerify('##.#'),
+            'description' => $this->faker->text,
+            'weight' => $this->faker->numerify('##.#'),
+            'stock' => $this->faker->numerify('###')
+        ];
+    }
+}
